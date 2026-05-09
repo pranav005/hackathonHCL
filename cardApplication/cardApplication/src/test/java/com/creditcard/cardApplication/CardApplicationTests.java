cac102fe7dfe0f7a3a7b1bcae01082f9f97f5854
pranav005 <5patelpranav@email.com>
2026-05-09 14:12:10 +0530

    initial commit

---
+++ cardApplication/cardApplication/src/test/java/com/creditcard/cardApplication/CardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditcard.cardApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
